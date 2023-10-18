5ae062d5b478474bdfa1c713d7eb113e278ba069
Gakamine <jolyantoine@pm.me>
2023-10-18 16:05:39 +0200

    Update Results.vue

--- src/pages/Results.vue
+++ src/pages/Results.vue
@@ -2,7 +2,7 @@
   <b-tabs type="is-toggle-rounded">
     <b-tab-item label="Tier A">
       <iframe
-        src="https://challonge.com/en/6oaq7bkv/module"
+        src="https://challonge.com/en/k0repnem/module"
         width="100%"
         height="500"
         frameborder="0"
@@ -12,7 +12,7 @@
     </b-tab-item>
     <b-tab-item label="Tier B">
       <iframe
-        src="https://challonge.com/en/bj4lpoi/module"
+        src="https://challonge.com/en/s4g845d9/module"
         width="100%"
         height="500"
         frameborder="0"
@@ -22,7 +22,17 @@
     </b-tab-item>
     <b-tab-item label="Tier Meme">
       <iframe
-        src="https://challonge.com/en/8vjsy6wl/module"
+        src="https://challonge.com/en/3rt83aiz/module"
+        width="100%"
+        height="500"
+        frameborder="0"
+        scrolling="auto"
+        allowtransparency="true"
+      ></iframe>
+    </b-tab-item>
+    <b-tab-item label="Husbando">
+      <iframe
+        src="https://challonge.com/en/ve7twwn1/module"
         width="100%"
         height="500"
         frameborder="0"
