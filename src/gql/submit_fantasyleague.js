7f4b4d8f35b3bb92d1735b4908e80301a989e319
Gakamine <jolyantoine@pm.me>
2023-10-26 13:46:09 +0200

    Update submit_fantasyleague.js

--- src/gql/submit_fantasyleague.js
+++ src/gql/submit_fantasyleague.js
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag'
 export const SUBMIT_FANTASYLEAGUE = gql`
-mutation submitFantasyleagueVote($token: String!, $waifus: [Int!]!, $best_waifu: Int!, $memes: [Int!]!, $best_meme: Int!, husbandos: [Int!]!, $best_husbando: Int!) {
+mutation submitFantasyleagueVote($token: String!, $waifus: [Int!]!, $best_waifu: Int!, $memes: [Int!]!, $best_meme: Int!, $husbandos: [Int!]!, $best_husbando: Int!) {
   submitFantasyleagueVote(
     input: {
       token: $token
