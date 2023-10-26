01b11e7dad01a7d3d64f9d11a14b9d017bfcb2b5
Gakamine <jolyantoine@pm.me>
2023-10-26 12:31:42 +0200

    Update WinnersPage.vue

--- src/pages/WinnersPage.vue
+++ src/pages/WinnersPage.vue
@@ -21,8 +21,8 @@
     <b-tooltip position="is-top" multilined>
       <img class="waifus" src="@/assets/w_2021.png" />
       <template v-slot:content>
-        <p><b>Holo</b></p>
-        <i>Waifu Wars 2019</i>
+        <p><b>Yor Forger</b></p>
+        <i>Waifu Wars 2021</i>
       </template>
     </b-tooltip>
   </div>
