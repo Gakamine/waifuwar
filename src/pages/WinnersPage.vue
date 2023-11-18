424c803ddf9ccf04354a8c0dc05236da2397a0d5
Gakamine <jolyantoine@pm.me>
2023-11-18 22:00:56 +0100

    new result layout

--- src/pages/WinnersPage.vue
+++ src/pages/WinnersPage.vue
@@ -4,34 +4,60 @@
       backgroundImage: 'url(' + require('@/assets/architecture.png') + ')',
     }"
   >
-    <b-tooltip position="is-top" multilined>
-      <img class="waifus" src="@/assets/w_2019.png" />
-      <template v-slot:content>
-        <p><b>Holo</b></p>
-        <i>Waifu Wars 2019</i>
-      </template>
-    </b-tooltip>
-    <b-tooltip position="is-top" multilined>
-      <img class="waifus" src="@/assets/w_2020.png" />
-      <template v-slot:content>
-        <p><b>Makise Kurisu</b></p>
-        <i>Waifu Wars 2020</i>
-      </template>
-    </b-tooltip>
-    <b-tooltip position="is-top" multilined>
-      <img class="waifus" src="@/assets/w_2021.png" />
-      <template v-slot:content>
-        <p><b>Violet Evergarden</b></p>
-        <i>Waifu Wars 2021</i>
-      </template>
-    </b-tooltip>
-    <b-tooltip position="is-top" multilined>
-      <img class="waifus" src="@/assets/w_2022.png" />
-      <template v-slot:content>
-        <p><b>Yor Forger</b></p>
-        <i>Waifu Wars 2022</i>
-      </template>
-    </b-tooltip>
+  <b-carousel :arrow="true" :indicator="false">
+        <b-carousel-item>
+            <section class="hero is-medium">
+                <div class="hero-body has-text-centered">
+                  <b-tooltip position="is-top" multilined>
+                    <img class="waifus" src="@/assets/w_2019.png" />
+                    <template v-slot:content>
+                      <p><b>Holo</b></p>
+                      <i>Waifu Wars 2019</i>
+                    </template>
+                  </b-tooltip>
+                </div>
+            </section>
+          </b-carousel-item>
+          <b-carousel-item>
+            <section class="hero is-medium">
+                <div class="hero-body has-text-centered">
+                  <b-tooltip position="is-top" multilined>
+                    <img class="waifus" src="@/assets/w_2020.png" />
+                    <template v-slot:content>
+                      <p><b>Makise Kurisu</b></p>
+                      <i>Waifu Wars 2020</i>
+                    </template>
+                  </b-tooltip>
+                </div>
+            </section>
+        </b-carousel-item>
+        <b-carousel-item>
+            <section class="hero is-medium">
+                <div class="hero-body has-text-centered">
+                  <b-tooltip position="is-top" multilined>
+                    <img class="waifus" src="@/assets/w_2021.png" />
+                    <template v-slot:content>
+                      <p><b>Violet Evergarden</b></p>
+                      <i>Waifu Wars 2021</i>
+                    </template>
+                  </b-tooltip>
+                </div>
+            </section>
+        </b-carousel-item>
+        <b-carousel-item>
+            <section class="hero is-medium">
+                <div class="hero-body has-text-centered">
+                  <b-tooltip position="is-top" multilined>
+                    <img class="waifus" src="@/assets/w_2022.png" />
+                    <template v-slot:content>
+                      <p><b>Yor Forger</b></p>
+                      <i>Waifu Wars 2022</i>
+                    </template>
+                  </b-tooltip>
+                </div>
+            </section>
+        </b-carousel-item>
+    </b-carousel>
   </div>
 </template>
 <script>
